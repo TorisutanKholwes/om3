771d52184f03508529f46bf3a5f5f65ac0802fbe
Tristan Clowez <tristan.clowez@opensvc.com>
2026-06-24 10:33:27 +0200

    Fix the "sync restore" command cobra flag definition

--- core/ox/factory.go
+++ core/ox/factory.go
@@ -2660,7 +2660,7 @@ func newCmdObjectInstanceSyncRestore(kind string) *cobra.Command {
 	flags := cmd.Flags()
 	addFlagsGlobal(flags, &options.OptsGlobal)
 	commoncmd.FlagsLock(flags, &options.OptsLock)
-	commoncmd.FlagsResourceSelector(flags, &options.OptsResourceSelector)
+	commoncmd.FlagsResourceSelector(cmd, &options.OptsResourceSelector)
 	commoncmd.FlagForce(flags, &options.Force)
 	return cmd
 }
